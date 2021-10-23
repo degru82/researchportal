ec0ed4ede58348aa9228726c3b86e00cb40d4587
Sewook Kim <degru82@gmail.com>
2021-10-23 16:06:20 +0900

    added __str__ function to FeedItem model to show list properly

--- rssfeeds/models.py
+++ rssfeeds/models.py
@@ -24,3 +24,6 @@ class FeedItem(models.Model):
     id = models.UUIDField(
         default=uuid.uuid4, unique=True, primary_key=True, editable=False
     )
+
+    def __str__(self):
+        return self.title
