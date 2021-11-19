13bfb1f50af520f387071da2f19ccdf0c28dc2e3
Sewook Kim <degru82@gmail.com>
2021-11-19 13:00:24 +0900

    changed url scheme

--- rssfeeds/urls.py
+++ rssfeeds/urls.py
@@ -2,21 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcomePageView, name='welcome-page'),
-    path('channel/', views.show_channels, name='channel-list'),
-    path('channel/new', views.enroll_channel, name='new-channel'),
+    path('', views.show_channels, name='channel-list'),
+    path('new', views.enroll_channel, name='new-channel'),
     path(
-        'channel/<str:channel_id>/',
+        '<str:channel_id>',
         views.show_singlechannel,
         name='channel-detail'
     ),
     path(
-        'channel/update/<str:channel_id>/',
+        'update/<str:channel_id>/',
         views.update_channel,
         name='update-channel'
     ),
     path(
-        'channel/delete/<str:channel_id>/',
+        'delete/<str:channel_id>/',
         views.delete_channel,
         name='delete-channel'
     ),
